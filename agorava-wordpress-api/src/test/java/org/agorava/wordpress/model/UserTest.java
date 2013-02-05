5db02e6306a879e6a8e94bd0e2025d1f9e717417
Nicolas Delsaux <nicolas.delsaux@gmail.com>
2013-02-05 21:48:13 +0100

    first json mapping test

---
+++ agorava-wordpress-api/src/test/java/org/agorava/wordpress/model/UserTest.java
@@ -0,0 +1,16 @@
+package org.agorava.wordpress.model;
+
+/**
+ * TODO check how well (or not) user json file maps to java class
+ */
+public class UserTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
